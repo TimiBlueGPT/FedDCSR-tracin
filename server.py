783b4ee100e9d55378520a457b1f9f220c95d11b
CandyGreen <1311833902@qq.com>
2025-09-21 18:38:34 +0800

    1

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 from collections import OrderedDict
 from collections import defaultdict
+import copy
 
 class ClientAttributor:
     def __init__(self):
@@ -18,7 +19,9 @@ class ClientAttributor:
 class Server(object):
     def __init__(self, args, init_global_params):
         self.args = args
-        self.global_params = init_global_params
+        self.global_params = self._clone_param_list(init_global_params)
+        self.latest_progress_direction = None
+        self.latest_client_scores = {}
         self.attributor = ClientAttributor()
         if args.method == "FedDCSR":
             self.global_reps = None
@@ -37,8 +40,10 @@ class Server(object):
         """
         # Record the model parameter aggregation results of each branch
         # separately
+        prev_global_params = self._clone_param_list(self.global_params)
         num_branchs = len(self.global_params)
         print(num_branchs)
+        new_global_params = [None] * num_branchs
         for branch_idx in range(num_branchs):
             client_params_sum = None
             for c_id in random_cids:
@@ -46,15 +51,25 @@ class Server(object):
                 current_client_params = clients[c_id].get_params()[branch_idx]
                 # Sum it up with weights
                 if client_params_sum is None:
-                    client_params_sum = dict((key, value
-                                              * clients[c_id].train_weight)
-                                             for key, value
-                                             in current_client_params.items())
+                    client_params_sum = OrderedDict(
+                        (key, current_client_params[key]
+                         * clients[c_id].train_weight)
+                        for key in current_client_params.keys())
                 else:
                     for key in client_params_sum.keys():
-                        client_params_sum[key] += clients[c_id].train_weight \
-                            * current_client_params[key]
-            self.global_params[branch_idx] = client_params_sum
+                        client_params_sum[key] += (
+                                clients[c_id].train_weight
+                                * current_client_params[key])
+            if client_params_sum is None:
+                client_params_sum = self._clone_param_branch(
+                    prev_global_params[branch_idx])
+            new_global_params[branch_idx] = client_params_sum
+
+        self.global_params = new_global_params
+        progress_direction = self._compute_progress_direction(
+            prev_global_params, self.global_params)
+        self.latest_progress_direction = progress_direction
+        self._update_progress_scores(clients, random_cids, progress_direction)
 
     def aggregate_reps(self, clients, random_cids):
         """Sums up representations of user sequences shared by all active
@@ -100,3 +115,68 @@ class Server(object):
         """Returns a reference to the parameters of the global model.
         """
         return self.global_reps
+
+
+    @staticmethod
+    def _clone_param_branch(branch):
+        if branch is None:
+            return None
+        cloned = OrderedDict()
+        for key, value in branch.items():
+            if isinstance(value, torch.Tensor):
+                cloned[key] = value.clone()
+            else:
+                cloned[key] = copy.deepcopy(value)
+        return cloned
+
+    @classmethod
+    def _clone_param_list(cls, params):
+        if params is None:
+            return None
+        return [cls._clone_param_branch(branch) for branch in params]
+
+    @staticmethod
+    def _compute_progress_direction(prev_params, new_params):
+        if (prev_params is None) or (new_params is None):
+            return None
+        progress_direction = []
+        for prev_branch, new_branch in zip(prev_params, new_params):
+            branch_direction = OrderedDict()
+            if (prev_branch is None) or (new_branch is None):
+                progress_direction.append(branch_direction)
+                continue
+            for key, new_value in new_branch.items():
+                prev_value = prev_branch[key]
+                branch_direction[key] = -(new_value - prev_value)
+            progress_direction.append(branch_direction)
+        return progress_direction
+
+    def _update_progress_scores(self, clients, random_cids, progress_direction):
+        if (progress_direction is None) or (len(random_cids) == 0):
+            self.latest_client_scores = {}
+            return
+
+        round_scores = {}
+        for c_id in random_cids:
+            client_grads = clients[c_id].get_grads()
+            score = 0.0
+            for branch_grad, branch_direction in zip(client_grads, progress_direction):
+                if branch_grad is None:
+                    continue
+                for key, grad_val in branch_grad.items():
+                    if key not in branch_direction:
+                        continue
+                    score += float(torch.sum(grad_val * branch_direction[key]))
+            round_scores[c_id] = score
+            self.attributor.add_score(c_id, score)
+
+        self.latest_client_scores = round_scores
+
+    def get_latest_progress_scores(self):
+        return self.latest_client_scores
+
+    def get_progress_direction(self):
+        return self.latest_progress_direction
+
+    def get_topk_contributors(self, k=20):
+        return self.attributor.dump_topk(k)
